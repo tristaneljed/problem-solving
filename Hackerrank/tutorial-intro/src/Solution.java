9fc441486652bff75483a13e5b2cdc678dd07a12
Siddharth Goel <siddharth98391@gmail.com>
2015-02-12 11:03:39 +0800

    [Hackerrank] Some more trivial problems
    Signed-off-by: Siddharth Goel <siddharth98391@gmail.com>

---
+++ Hackerrank/tutorial-intro/src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int v = s.nextInt();
+		int n = s.nextInt();
+		int index = -1;
+		int [] ar = new int[n];
+		for (int i=0; i<n; i++) {
+			ar[i] = s.nextInt();
+			if (ar[i] == v) {
+				index = i;
+			}
+		}
+		System.out.println(index);
+		s.close();
+	}
+
+}
